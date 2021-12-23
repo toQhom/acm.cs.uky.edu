defb9c12c837ed8ff602f6406a213230a32f662b
Matt Mitchell <matt.mitchell@uky.edu>
2021-12-23 14:54:31 -0500

    Added social media buttons to cathacks header menu

--- cathacks/app/js/main.js
+++ cathacks/app/js/main.js
@@ -6,16 +6,33 @@ $(function() {
             $('#navbar').css("display", "block"); // Always display navbar for now.
         }
     });
-    $("#navbar li").click(function(e) {
-        e.preventDefault();
-        var idName = "#" + $(this).attr('id');
-        idName = idName.substring(0, idName.length-2);
-        console.log(idName);
-        $('html, body').animate({
-            scrollTop: $(idName).offset().top - 50
-        }, 600);
-    });
 		
+	
+    $("#Homeli").click(function(e) {
+			e.preventDefault();
+			$('html, body').animate({
+            scrollTop: $('#Home').offset().top - 50
+            }, 600);		
+		});
+    $("#Aboutli").click(function(e) {
+			e.preventDefault();
+			$('html, body').animate({
+            scrollTop: $('#About').offset().top - 50
+            }, 600);		
+		});
+    $("#Applyli").click(function(e) {
+			e.preventDefault();
+			$('html, body').animate({
+            scrollTop: $('#Apply').offset().top - 50
+            }, 600);		
+		});
+    $("#Sponsorli").click(function(e) {
+			e.preventDefault();
+			$('html, body').animate({
+            scrollTop: $('#Sponsor').offset().top - 50
+            }, 600);		
+		});	
+	
 });
 
 $(".main-btnBannerBtn").click(function(e) {
